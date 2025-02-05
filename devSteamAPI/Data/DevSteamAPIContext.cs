75231143346d28cae15792513b161c04f15fbee0
Gabriela <gabrielasiqueiracaputti@gmail.com>
2025-02-05 16:07:14 -0300

    Adicionado os Controladores

--- devSteamAPI/Data/DevSteamAPIContext.cs
+++ devSteamAPI/Data/DevSteamAPIContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using devSteamAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace devSteamAPI.Data
@@ -10,10 +11,16 @@ namespace devSteamAPI.Data
         {
         }
 
+        //Definir as tabelas do banco de dados// como o modelo vai ser referênciado no sistema
+        public DbSet<Jogo> Jogos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
         //Sobrescrever o método OnModelCreating
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Jogo>().ToTable("Jogos");
+            modelBuilder.Entity<Categoria>().ToTable("Categorias");
         }
     }
 }
---
+++ devSteamAPI/Models/Jogo.cs
@@ -0,0 +1,13 @@
+﻿namespace devSteamAPI.Models
+{
+    public class Jogo
+    {
+        public Guid JogoId { get; set; }
+        public string Nome { get; set; }
+        public int Classificacao { get; set; }
+        public string? Descricao { get; set; } //? é não o obrigatório
+        public string? Imagem { get; set; } // salvamos o caminho da imagem e ai ela busca sozinho
+        public Guid CategoriaId { get; set; }
+        public Categoria? Categoria { get; set; }
+    }
+}
